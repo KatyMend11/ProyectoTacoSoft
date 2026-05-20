7f1f6a5f9f127a685a2bb5f80675dffbe4af1d79
JosephMolinaGTS <josephm0604@gmail.com>
2026-05-19 19:42:27 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,20 @@
 require('dotenv').config();
 const sql = require('mssql');
+
+const config = {
+    user: process.env.DB_USER || 'ElSinaloense',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'ElSinaloensePOS',
+    server: process.env.DB_SERVER || 'DESKTOP-SUIFTH5',
+    driver: 'msnodesqlv8',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true,
+        enableArithAbort: true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+};
